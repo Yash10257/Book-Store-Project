9a4ef509c25b5975ef2cf820901c5a658f08f8a3
Yash Mishra <78741580+Yash10257@users.noreply.github.com>
2023-10-21 19:55:04 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT=5555;
 
-export const mongoDBURL='mongodb+srv://root:mainhibtaunga@bs-mern.3kfg7fe.mongodb.net/books-collection?retryWrites=true&w=majority'
+export const mongoDBURL='idhar apni mongodb atlas ki URI paste kriye'
